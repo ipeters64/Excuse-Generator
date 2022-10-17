c0d25ffb8b27ce3188ad60358919e8bd8f334241
ipeters123 <isaiah.peters64@gmail.com>
2022-10-17 17:39:02 +0000

    Updated Excuse Generator Button

--- src/app.js
+++ src/app.js
@@ -2,10 +2,35 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuseGen();
+  });
+  console.log("hello");
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let excuseGen = () => {
+  let pronoun = ["The ", "A "];
+  let subject = ["dog ", "cat ", "stranger ", "friend ", "homeless man "];
+  let verb = ["ate ", "stole ", "ran away with my ", "peed on ", "chewed on "];
+  let item = ["homework ", "assignment ", "notes "];
+  let place = ["at my home ", "in my yard ", "in my garden "];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let verbIndex = Math.floor(Math.random() * verb.length);
+  let itemIndex = Math.floor(Math.random() * item.length);
+  let placeIndex = Math.floor(Math.random() * place.length);
+
+  return (
+    pronoun[pronounIndex] +
+    "" +
+    subject[subjectIndex] +
+    "" +
+    verb[verbIndex] +
+    "" +
+    item[itemIndex] +
+    "" +
+    place[placeIndex]
+  );
 };
